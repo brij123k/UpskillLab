8b83e70a536d200e49d595df9c30f4ba00b6dab2
Abhishek Maurya <163228483+AbhishekMaurya63@users.noreply.github.com>
2025-04-13 13:47:18 +0530

    Update AdmissionForm.jsx

--- client/src/components/Forms/AdmissionForm.jsx
+++ client/src/components/Forms/AdmissionForm.jsx
@@ -95,7 +95,7 @@ const AdmissionForm = () => {
   });
 
   return (
-    <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8" id='AdmissionForm'>
+    <div className="bg-gray-50 py-12 px-4 sm:px-6 lg:px-8" id='AdmissionForm'>
       <div className="max-w-6xl mx-auto">
         {/* Header */}
         <div className="text-center mb-12">
