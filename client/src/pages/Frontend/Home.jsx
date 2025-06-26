403b6f440b6712e8ecb74c15aad6dd135f04e0e8
Abhishek Maurya <abhishek639398maurya@gmail.com>
2025-06-26 22:51:59 +0530

    added changes

--- client/src/pages/Frontend/Home.jsx
+++ client/src/pages/Frontend/Home.jsx
@@ -152,8 +152,8 @@ function Home() {
   ];
   return (
     <>
-    {/* <ExitIntentModal/> */}
-
+    {/* <ExitIntentModal/>
+{/*
           <!-- Google Tag Manager -->
       <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -161,7 +161,7 @@ function Home() {
       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
       })(window,document,'script','dataLayer','GTM-P6TQCRLK');</script>
       <!-- End Google Tag Manager -->
-
+ */}
       <div className='min-h-fit'>
         <CarouselContainer />
       </div>
